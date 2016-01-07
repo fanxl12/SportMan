55da5dc12e21d112bb6c66f17473710b49ca36ac
fanxl12 <744822514@qq.com>
2016-01-07 14:19:22 +0800

    1.更改CourseSubFragment的导包; 2.增加自动更新功能; 3.增加检查更新功能; 4.增加分享功能; 5.修改筛选功能;

---
+++ app/src/main/java/com/agitation/sportman/adapter/RightTestMenuAdapter.java
@@ -0,0 +1,29 @@
+package com.agitation.sportman.adapter;
+
+import android.content.Context;
+
+import com.agitation.sportman.R;
+import com.agitation.sportman.utils.ViewHolder;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Fanxl on 2015/12/26.
+ */
+public class RightTestMenuAdapter extends CommonAdapter<Map<String, Object>> {
+
+    public void setData(List<Map<String, Object>> mDatas){
+        this.mDatas=mDatas;
+        this.notifyDataSetChanged();
+    }
+
+    public RightTestMenuAdapter(Context context, List<Map<String, Object>> mDatas, int itemLayoutId) {
+        super(context, mDatas, itemLayoutId);
+    }
+
+    @Override
+    public void convert(ViewHolder helper, Map<String, Object> item) {
+        helper.setText(R.id.right_item_name, item.get("name")+"");
+    }
+}
